3c5591e5cea226be4da00a2a38f56d92b3b34252
Marlon Couto <marloncouto.dev@gmail.com>
2022-12-14 10:42:08 -0300

    Finaliza requisito 1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,57 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom/cjs/react-router-dom.min';
+
+import Login from './pages/Login';
+import Profile from './pages/Profile';
+import Favorites from './pages/Favorites';
+import Album from './pages/Album';
+import Search from './pages/Search';
+import NotFound from './pages/NotFound';
+import ProfileEdit from './pages/ProfileEdit';
 
 class App extends React.Component {
   render() {
-    return (<p>TrybeTunes</p>);
+    return (
+      <div>
+        <p>TrybeTunes</p>
+        <Switch>
+          <Route
+            exact
+            path="/"
+            component={ Login }
+          />
+          <Route
+            exact
+            path="/search"
+            component={ Search }
+          />
+          <Route
+            exact
+            path="/album/:id"
+            component={ Album }
+          />
+          <Route
+            exact
+            path="/favorites"
+            component={ Favorites }
+          />
+          <Route
+            exact
+            path="/profile"
+            component={ Profile }
+          />
+          <Route
+            exact
+            path="/profile/edit"
+            component={ ProfileEdit }
+          />
+          <Route
+            path="*"
+            component={ NotFound }
+          />
+        </Switch>
+      </div>
+    );
   }
 }
 
